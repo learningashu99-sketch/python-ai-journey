492205cf0a3155da31620e2cbd763dceb40b34c4
Ashwani <learningashu99@gmail.com>
2026-03-09 22:53:48 +0530

    done statistics_functions today

---
+++ week1_numpy/statistics_functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+data = np.array([10, 20, 30, 40, 50])
+
+print("Dataset:", data)
+
+print("Sum:", np.sum(data))
+print("Mean:", np.mean(data))
+print("Max:", np.max(data))
+print("Min:", np.min(data))
+print("Standard Deviation:", np.std(data))
